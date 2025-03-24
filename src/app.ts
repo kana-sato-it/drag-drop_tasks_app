0e5d58a938185e0a053a73825b83c6dc439e8856
Kana Sato <kanasato.it@gmail.com>
2025-03-24 22:47:55 +0900

    added submitHandler

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,38 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    mandayInputElement: HTMLInputElement;
 
+    //要素への参照
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.mandayInputElement = this.element.querySelector('#manday') as HTMLInputElement;
+        this.configure();
         this.attach();
     }
+    // イベントハンドラ
+    private submitHandler(event: Event)
+    {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
 
+    //  イベントの設定
+    private configure()
+    {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+
+    // 要素の追加
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
