1f593a496415e5a899c0dd6f57c3eac0093daeaa
Kana Sato <kanasato.it@gmail.com>
2025-03-27 21:37:29 +0900

    move drag & drop interfaces to a separate file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,4 @@
-// Drag & Drop Interfaces
-interface Draggable {
-  dragStartHandler(event: DragEvent): void;
-  dragEndHandler(event: DragEvent): void;
-}
-
-interface DragTarget {
-  dragOverHandler(event: DragEvent): void;
-  dropHandler(event: DragEvent): void;
-  dragLeaveHandler(event: DragEvent): void;
-}
+/// <reference path="drag-drop-interfaces.ts" />
 
 //Project Type
 enum ProjectStatus {
