c0d237da6d27cdde5b02921b8637609fc530a7ca
kana-sato-it <148974734+kana-sato-it@users.noreply.github.com>
2025-04-06 23:15:26 +0900

    autobind.ts

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,4 +1,3 @@
-// autobind decorator
 export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
